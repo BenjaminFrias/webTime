46a6d6c1998d804cd85ee3eee673141c0af19197
benjaminfrias <benjaminfrias2004@gmail.com>
2025-03-25 16:15:20 -0400

    refactor: separate timer logic

--- src/content.js
+++ src/content.js
@@ -1,13 +1,6 @@
-function timer() {
+function startTimer(timerElem) {
 	let seconds = 0;
 	let minutes = 0;
-
-	// Create timer elements
-	const timerContainer = document.createElement("div");
-	timerContainer.classList.add("timer-container");
-	const timer = document.createElement("p");
-	timerContainer.appendChild(timer);
-	document.body.appendChild(timerContainer);
 
 	setInterval(() => {
 		seconds++;
@@ -17,10 +10,27 @@ function timer() {
 		}
 
 		// Update timer element
-		timer.textContent = `${minutes <= 9 ? `0${minutes}` : minutes}:${
+		timerElem.textContent = `${minutes <= 9 ? `0${minutes}` : minutes}:${
 			seconds <= 9 ? `0${seconds}` : seconds
 		}`;
 	}, 1000);
 }
 
-timer();
+function createTimerElem() {
+	const timerContainer = document.createElement("div");
+	timerContainer.classList.add("timer-container");
+
+	const timer = document.createElement("p");
+
+	timerContainer.appendChild(timer);
+	document.body.appendChild(timerContainer);
+
+	return timer;
+}
+
+function initializeTimer() {
+	const timerElem = createTimerElem();
+	startTimer(timerElem);
+}
+
+initializeTimer();
