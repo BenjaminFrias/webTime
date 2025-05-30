c6ba43f8f21ac5d37f0c776334862a9e9865b93a
benjaminfrias <benjaminfrias2004@gmail.com>
2025-05-30 18:31:53 -0400

    refactor(UI): modularize timerUI

--- src/content.js
+++ src/content.js
@@ -1,33 +1,4 @@
-class TimerUI {
-	constructor() {
-		this.timerElem = null;
-	}
-
-	createTimerElem() {
-		const timerContainer = document.createElement('div');
-		timerContainer.classList.add('timer-container');
-
-		const timer = document.createElement('p');
-		timer.style.fontFamily = "'Rubik', serif";
-
-		timerContainer.appendChild(timer);
-		document.body.appendChild(timerContainer);
-
-		this.timerElem = timer;
-	}
-
-	updateTimerElem({ hours, minutes, seconds }) {
-		let formmatedMin = minutes <= 9 ? `0${minutes}` : minutes;
-		let formmatedSec = seconds <= 9 ? `0${seconds}` : seconds;
-		let formmatedHours = hours <= 9 ? `0${hours}` : hours;
-
-		if (hours > 0) {
-			this.timerElem.textContent = `${formmatedHours}:${formmatedMin}:${formmatedSec}`;
-		} else {
-			this.timerElem.textContent = `${formmatedMin}:${formmatedSec}`;
-		}
-	}
-}
+import { TimerUI } from './utils/timerUI.js';
 
 const timerUI = new TimerUI();
 
