d73cde79775e4307eea93de801cfa35ad60141b6
benjaminfrias <benjaminfrias2004@gmail.com>
2025-06-03 15:48:45 -0400

    feat: update isTracked function to check for several tracked webs

--- src/utils/tab.js
+++ src/utils/tab.js
@@ -1,3 +1,6 @@
+import { getData } from './data.js';
+import { TRACKED_DATA_KEY } from '../settings.js';
+
 export async function getCurrentTab() {
 	const queryOptions = { active: true, currentWindow: true };
 	try {
@@ -8,6 +11,19 @@ export async function getCurrentTab() {
 	}
 }
 
-export function isYouTubeURL(url) {
-	return url && url.includes('youtube.com');
+export async function isTrackedURL(url) {
+	try {
+		const websites = await getData(TRACKED_DATA_KEY);
+
+		let isTracked = false;
+		Object.keys(websites).forEach((web) => {
+			if (url && url.includes(web)) {
+				isTracked = true;
+			}
+		});
+
+		return isTracked;
+	} catch (error) {
+		throw new Error('Error fetching websites data: ', error);
+	}
 }
