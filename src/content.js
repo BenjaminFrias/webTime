fbbfac34f0de3c0db36a29553ded6fdee5f9c8bc
benjaminfrias <benjaminfrias2004@gmail.com>
2025-06-07 17:13:20 -0400

    fix: resolve font display for multiple tracked websites

--- src/content.js
+++ src/content.js
@@ -11,8 +11,8 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 	if (request.type === 'createTimerElement') {
 		if (!timerUI.timerElem) {
+			// injectCustomFont();
 			timerUI.createTimerElem();
-			injectCustomFont();
 		}
 	}
 });
